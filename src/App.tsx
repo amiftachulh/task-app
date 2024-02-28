ce7b8d161e8a67b59d80ddd78acf033ec5856396
amiftachulh <miftachul.mkb@gmail.com>
2024-02-28 18:20:25 +0700

    fix: responsive svg background

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="fixed inset-0 z-[-1]">
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" width="1440" height="560" preserveAspectRatio="none" viewBox="0 0 1440 560">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" height="100%" preserveAspectRatio="xMidYMid slice" viewBox="0 0 1440 560">
           <g mask="url(&quot;#SvgjsMask1042&quot;)" fill="none" stroke="currentColor" strokeWidth="2" >
             <path d="M -837.8367546194895,246 C -741.84,274.6 -549.84,420.4 -357.83675461948945,389 C -165.84,357.6 -69.84,92.2 122.16324538051055,89 C 314.16,85.8 410.16,354.4 602.1632453805105,373 C 794.16,391.6 914.6,167.6 1082.1632453805105,182 C 1249.73,196.4 1368.43,392.4 1440,445" />
             <path d="M -298.8399860778011,72 C -202.84,152.4 -10.84,453.6 181.16001392219889,474 C 373.16,494.4 469.16,177.2 661.1600139221989,174 C 853.16,170.8 949.16,438.6 1141.160013922199,458 C 1333.16,477.4 1561.39,296.6 1621.160013922199,271 C 1680.93,245.4 1476.23,318.2 1440,330" />
